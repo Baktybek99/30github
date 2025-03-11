77e091d6b8e2e759f5e1946ed2307e5e6ec3e94d
Zumba <bktbkzmbkv@gmail.com>
2025-03-11 17:06:02 +0600

    web socket realization

---
+++ 30git/30git/WebSockets/WebSockerServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30git.WebSockets
+{
+    public class WebSockerServer
+    {
+        public static async Task ConnectAsync()
+        {
+            using (var client = new ClientWebSocket())
+            {
+                await client.ConnectAsync(new Uri("ws://localhost:8080/ws/"), CancellationToken.None);
+                Console.WriteLine("Подключено к серверу");
+
+                string message = "Привет, сервер!";
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                await client.SendAsync(new ArraySegment<byte>(bytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+                byte[] buffer = new byte[1024];
+                var result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                string response = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                Console.WriteLine($"Ответ от сервера: {response}");
+
+                await client.CloseAsync(WebSocketCloseStatus.NormalClosure, "Закрытие соединения", CancellationToken.None);
+            }
+        }
+    }
+}
---
+++ 30git/30git/WebSockets/WebSocketClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30git.WebSockets
+{
+    public class WebSocketClient
+    {
+        public static async Task StartAsync()
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/ws/");
+            listener.Start();
+            Console.WriteLine("WebSocket сервер запущен на ws://localhost:8080/ws/");
+
+            while (true)
+            {
+                var context = await listener.GetContextAsync();
+                if (context.Request.IsWebSocketRequest)
+                {
+                    var webSocketContext = await context.AcceptWebSocketAsync(null);
+                    await HandleConnection(webSocketContext.WebSocket);
+                }
+                else
+                {
+                    context.Response.StatusCode = 400;
+                    context.Response.Close();
+                }
+            }
+        }
+
+        private static async Task HandleConnection(System.Net.WebSockets.WebSocket webSocket)
+        {
+            byte[] buffer = new byte[1024];
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                if (result.MessageType == WebSocketMessageType.Close)
+                {
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Закрыто", CancellationToken.None);
+                    break;
+                }
+
+                string receivedMessage = Encoding.UTF8.GetString(buffer, 0, result.Count);
+                Console.WriteLine($"Получено сообщение: {receivedMessage}");
+
+                string responseMessage = $"Эхо: {receivedMessage}";
+                byte[] responseBytes = Encoding.UTF8.GetBytes(responseMessage);
+                await webSocket.SendAsync(new ArraySegment<byte>(responseBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+
+    }
+}
