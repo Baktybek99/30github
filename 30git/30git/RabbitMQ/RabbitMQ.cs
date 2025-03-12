5e152e32209c06437c1dcb442dd9423ffd20b609
Zumba <bktbkzmbkv@gmail.com>
2025-03-12 15:28:21 +0600

    impement Rabbit

---
+++ 30git/30git/RabbitMQ/RabbitMQ.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30git.RabbitMQ
+{
+    public class RabbitMQ
+    {
+        static async Task SendMessage(string queueName, string message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: queueName,
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+            var body = Encoding.UTF8.GetBytes(message);
+            await channel.BasicPublishAsync("", queueName, body);
+
+            Console.WriteLine($" [x] Sent: {message}");
+        }
+
+        static async Task ReceiveMessage(string queueName)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var connection = await factory.CreateConnectionAsync();
+            var channel = await connection.CreateChannelAsync();
+
+            await channel.QueueDeclareAsync(queue: queueName,
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+
+            var consumer = new AsyncEventingBasicConsumer(channel);
+            consumer.ReceivedAsync += async (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Console.WriteLine($" [x] Received: {message}");
+                await Task.Yield();
+            };
+
+            await channel.BasicConsumeAsync(queue: queueName,
+                                             autoAck: true,
+                                             consumer: consumer);
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+    }
+}
